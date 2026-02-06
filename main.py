402e2dbf5d25b9c4aaa199784db993cf11a1ea57
artyomturkoff <art.turkoff@gmail.com>
2026-02-06 17:04:00 +0100

    frame rate added

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+
 
     while True:
         log_state()
@@ -16,9 +20,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-    screen.fill("black")
-    pygame.display.flip()
+
+        dt = clock.tick(60) /1000
+        screen.fill("black")
+        pygame.display.flip()
 
 
 
