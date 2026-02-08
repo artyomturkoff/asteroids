b9a82cb873ff26f7983dc189a508daf545698745
artyomturkoff <art.turkoff@gmail.com>
2026-02-08 12:26:16 +0100

    player move added

--- main.py
+++ main.py
@@ -23,12 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) /1000
+        
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-       
 
+        dt = clock.tick(60) /1000     
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,22 +1,37 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
     # Triangle for drawing the Player
-def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
-
-def draw(self, screen):
-    pygame.draw.polygon(screen, "white", self.triangle(), width=LINE_WIDTH)
-
-
-
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    # Draw the white player's triangle
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=LINE_WIDTH)
+    
+    # Player rotation movement
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+    
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+        
