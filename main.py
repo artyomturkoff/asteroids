715c2d8a2d701b9af3ef4f00c4f4042c0fc6d900
artyomturkoff <art.turkoff@gmail.com>
2026-02-08 15:42:08 +0100

    minor changes to main.py

--- main.py
+++ main.py
@@ -8,29 +8,25 @@ from asteroidfield import AsteroidField
 from shot import Shot
 
 def main():    
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    asteroidfields = pygame.sprite.Group()
     shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroid = AsteroidField()
     Shot.containers = (shots, updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
-
+    dt = 0
+    
     while True:
         log_state()
 
